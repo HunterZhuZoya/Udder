0f2a900e1a190f51ef731f09ac9b932c14ca54db
Yingying Zhu <Yingying_Zhu@yingyings-macbook-pro.local>
2018-05-16 09:46:31 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Yingying Zhu on 5/16/18.
+//  Copyright © 2018 Yingying Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
